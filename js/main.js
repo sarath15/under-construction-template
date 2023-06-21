d95acf0faf413d81c4b41fef63a2b4696954ee59
sarath15 <sarath.v15@gmail.com>
2023-06-21 17:57:38 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -50,7 +50,7 @@ $(window).load(function() {
 /* END ------------------------------------------------------- */
 
 $("#countdown").countdown({
-  date: "Jan 01, 2022 12:01:00",
+  date: "Nov 01, 2023 12:01:00",
   render: function(data) {
     var el = $(this.el);
     el.empty()
